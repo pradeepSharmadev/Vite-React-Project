1bf5b6c66851a373c52f533549f78285e3a391cb
pradeepSharmadev <amarahipradeep7319@gmail.com>
2024-01-14 00:58:14 +0530

    #

--- game-block/src/MyComponents.tsx
+++ game-block/src/MyComponents.tsx
@@ -1,9 +1,11 @@
 import Messase from './assets/Message'
+import Dynamic from './assets/Dynamic';
 
 const Main = () => {
     return(
         <div>
             <Messase />
+            <Dynamic />
         </div>
     )
 }
---
+++ game-block/src/assets/Dynamic.tsx
@@ -0,0 +1,16 @@
+
+const Dynamic = ()=>{
+    let name = "Pradeep Kumar"
+    let password = "Pradeep123"
+    if (password === "Pradeep123"){
+        return(
+            <>Hello {name}, This is your world of 1234 Numbers or your are Mathematics lover.</>
+        )
+    }else{
+        return(
+            <>Hello {name}, I hope You are doing well its my turn ABC</>
+        )
+    };
+};
+
+export default Dynamic;
